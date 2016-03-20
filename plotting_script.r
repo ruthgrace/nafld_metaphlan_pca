eaa4b447ebb7630eb7481db22a3fe34b21478419
ruthgrace <ruthgracewong@gmail.com>
2016-03-19 22:14:31 -0400

    added genus level biplots

--- plotting_script.r
+++ plotting_script.r
@@ -27,7 +27,33 @@ rownames(d) <- sub("_"," ",rownames(d))
 sample.sum <- apply(d,2,sum)
 one.percent <- sample.sum*0.01
 
+# get genus level for metaphlan
+species <- rownames(d)
+genus <- str_extract(species,"^[A-Za-z]*")
+
+d.genus <- aggregate(d,list(genus),sum)
+rownames(d.genus) <- d.genus$Group.1
+d.genus <- d.genus[,c(2:ncol(d.genus))]
+colnames(d.genus) <- str_extract(colnames(d.genus),"^[A-Z]*_[0-9]*")
+
+# get genus level for 16S
+otu.tab <- read.table("data/td_OTU_tag_mapped_lineage_working.txt", header=T, sep="\t", row.names=1, comment.char="", check.names=FALSE)
+
+colnames(otu.tab) <- gsub("[.]","_",colnames(otu.tab))
+
+taxonomy <- otu.tab$taxonomy
+
+otu.tab <- otu.tab[,c(1:(ncol(otu.tab)-1))]
+otu.genus <- c(as.character(taxonomy))
+
+for (i in c(1:length(taxonomy))) {
+  otu.genus[i] <- strsplit(otu.genus[i],c(";"))[[1]][6]
+}
+
+# adjust zeros
 d.adj.zero <- t(cmultRepl(t(d),method="CZM"))
+d.genus.adj.zero <- t(cmultRepl(t(d.genus),method="CZM"))
+otu.tab.genus.adj.zero <- t(cmultRepl(t(otu.tab.genus),method="CZM"))
 
 filter <- apply(d,1,function(x) length(which(x > one.percent)))
 d.filter <- d.adj.zero[which(filter > 0),]
@@ -36,9 +62,13 @@ d.filter.counts <- d[which(filter>0),]
 
 d.adj.zero <- d.adj.zero[order(apply(d.adj.zero,1,sum),decreasing=TRUE),]
 d.filter <- d.filter[order(apply(d.filter,1,sum),decreasing=TRUE),]
+d.genus.adj.zero <- d.genus.adj.zero[order(apply(d.genus.adj.zero,1,sum),decreasing=TRUE),]
+otu.tab.genus.adj.zero <- otu.tab.genus.adj.zero[order(apply(otu.tab.genus.adj.zero,1,sum),decreasing=TRUE),]
 
 d.names <- rownames(d.adj.zero)
 d.filter.names <- rownames(d.filter)
+d.genus.names <- rownames(d.genus.adj.zero)
+otu.tab.names <- rownames(otu.tab.genus.adj.zero)
 
 taxa.col <- data.frame(as.character(rownames(d)),rownames(d))
 colnames(taxa.col) <- c("taxon","color")
@@ -46,16 +76,36 @@ taxa.col[,2] <- distinctColorPalette(length(taxa.col[,2]))
 
 taxa.filter.col <- data.frame(as.character(rownames(d.filter)),rownames(d.filter))
 colnames(taxa.filter.col) <- c("taxon","color")
-taxa.filter.col[,2] <- distinctColorPalette(length(taxa.filter.col[,2]))
+taxa.filter.col[,2] <- taxa.col[match(taxa.filter.col[,1],taxa.col[,1]),2]
+
+all.genus <- unique(c(rownames(d.genus),rownames(otu.tab.genus)))
+all.genus.colors <- distinctColorPalette(length(all.genus))
+
+taxa.d.genus.col <- data.frame(rownames(d.genus),rownames(d.genus))
+colnames(taxa.d.genus.col) <- c("taxon","color")
+taxa.d.genus.col[,2] <- all.genus.colors[match(rownames(d.genus),all.genus)]
+
+taxa.otu.tab.genus.col <- data.frame(rownames(otu.tab.genus),rownames(otu.tab.genus))
+colnames(taxa.otu.tab.genus.col) <- c("taxon","color")
+taxa.otu.tab.genus.col[,2] <- all.genus.colors[match(rownames(otu.tab.genus),all.genus)]
+
 
 d.prop <- apply(d.adj.zero,2,function(x){x/sum(x)})
 d.filter.prop <- apply(d.filter,2,function(x) {x/sum(x)})
+d.genus.prop <- apply(d.genus.adj.zero, 2,function(x) {x/sum(x)})
+otu.tab.genus.prop <- apply(otu.tab.genus.adj.zero, 2,function(x) {x/sum(x)})
+
 
 d.clr <- t(apply(d.prop,2,function(x){log(x) - mean(log(x))}))
 d.filter.clr <- t(apply(d.filter.prop,2,function(x){log(x) - mean(log(x))}))
+d.genus.clr <- t(apply(d.genus.prop,2,function(x){log(x) - mean(log(x))}))
+otu.tab.genus.clr <- t(apply(otu.tab.genus.prop,2,function(x){log(x) - mean(log(x))}))
+
 
 d.pcx <- prcomp(d.clr)
 d.filter.pcx <- prcomp(d.filter.clr)
+d.genus.pcx <- prcomp(d.genus.clr)
+otu.tab.genus.pcx <- prcomp(otu.tab.genus.clr)
 
 conds <- data.frame(c(rep("NASH",10),rep("Healthy",10)))
 colnames(conds) <- "cond"
@@ -82,6 +132,22 @@ ylab=paste("PC2 ", round (sum(d.filter.pcx$sdev[2]^2)/mvar(d.filter.clr),3), sep
 xlabs.col=c(rep("red",10),rep("black",10)),
 expand=0.8,var.axes=T, scale=1, main="Biplot")
 barplot(d.filter.pcx$sdev^2/mvar(d.filter.clr),  ylab="variance explained", xlab="Component", main="Scree plot") # scree plot
+
+coloredBiplot(d.genus.pcx, cex=c(0.6, 0.6),
+arrow.len=0.05,
+xlab=paste("PC1 ", round (sum(d.genus.pcx$sdev[1]^2)/mvar(d.genus.clr),3), sep=""),
+ylab=paste("PC2 ", round (sum(d.genus.pcx$sdev[2]^2)/mvar(d.genus.clr),3), sep=""),
+xlabs.col=c(rep("red",10),rep("black",10)),
+expand=0.8,var.axes=T, scale=1, main="Biplot")
+barplot(d.genus.pcx$sdev^2/mvar(d.genus.clr),  ylab="variance explained", xlab="Component", main="Scree plot") # scree plot
+
+coloredBiplot(otu.tab.genus.pcx, cex=c(0.6, 0.6),
+arrow.len=0.05,
+xlab=paste("PC1 ", round (sum(otu.tab.genus.pcx$sdev[1]^2)/mvar(otu.tab.genus.clr),3), sep=""),
+ylab=paste("PC2 ", round (sum(otu.tab.genus.pcx$sdev[2]^2)/mvar(otu.tab.genus.clr),3), sep=""),
+xlabs.col=c(rep("red",10),rep("black",10)),
+expand=0.8,var.axes=T, scale=1, main="Biplot")
+barplot(otu.tab.genus.pcx$sdev^2/mvar(otu.tab.genus.clr),  ylab="variance explained", xlab="Component", main="Scree plot") # scree plot
 
 dev.off()
 
@@ -182,29 +248,6 @@ dev.off()
 
 # COMPARE EFFECT SIZE WITH 16S
 
-# get genus level for metaphlan
-species <- rownames(d)
-genus <- str_extract(species,"^[A-Za-z]*")
-
-d.genus <- aggregate(d,list(genus),sum)
-rownames(d.genus) <- d.genus$Group.1
-d.genus <- d.genus[,c(2:ncol(d.genus))]
-colnames(d.genus) <- str_extract(colnames(d.genus),"^[A-Z]*_[0-9]*")
-
-# get genus level for 16S
-otu.tab <- read.table("data/td_OTU_tag_mapped_lineage_working.txt", header=T, sep="\t", row.names=1, comment.char="", check.names=FALSE)
-
-colnames(otu.tab) <- gsub("[.]","_",colnames(otu.tab))
-
-taxonomy <- otu.tab$taxonomy
-
-otu.tab <- otu.tab[,c(1:(ncol(otu.tab)-1))]
-otu.genus <- c(as.character(taxonomy))
-
-for (i in c(1:length(taxonomy))) {
-  otu.genus[i] <- strsplit(otu.genus[i],c(";"))[[1]][6]
-}
-
 # read metadata
 MyMeta<- read.table("../exponentUnifrac/data/nash_data/metadata.txt", header=T, sep="\t", row.names=1, comment.char="", check.names=FALSE)
 metadata <- MyMeta[grepl("a$",rownames(MyMeta)),]
@@ -269,20 +312,36 @@ h.metnash.d <- d.genus
 
 h.metnash.d.aldex <- aldex(data.frame(h.metnash.d),as.character(d.cond))
 
+d.select <- d.genus
+suspect.samples <- c("HLD_80","HLD_85","CL_165")
+d.select <- d.genus[,which(!(colnames(d) %in% suspect.samples))]
+d.select <- d.select[which(apply(d.select,1,sum)>0),]
+d.select.cond <- colnames(d.select)
+d.select.cond <- sub("CL.*$","NASH Metagenomic",d.select.cond)
+d.select.cond <- sub("HLD.*$","Healthy Metagenomic",d.select.cond)
+
+h.metnash.d.select.aldex <- aldex(data.frame(d.select),d.select.cond)
+
 mycolor <- c(col2rgb("turquoise4"))
 red <- mycolor[1]
 green <- mycolor[2]
 blue <- mycolor[3]
 mycolor <- rgb(red/255, green/255, blue/255, 0.3)
 
+pdf("metaphlan_vs_16S_effect_sizes.pdf")
 common.genus <- rownames(h.metnash.d.aldex)[which(rownames(h.metnash.d.aldex) %in% rownames(h.metnash.aldex))]
-
 otu.tab.common.effect <- h.metnash.aldex$effect[match(common.genus,rownames(h.metnash.aldex))]
 d.common.effect <- h.metnash.d.aldex$effect[match(common.genus,rownames(h.metnash.d.aldex))]
-
-pdf("metaphlan_vs_16S_effect_sizes.pdf")
 plot(otu.tab.common.effect, d.common.effect, pch=19,col=mycolor, main="Effect sizes of healthy vs extreme NASH\nfor MetaPhlAn results vs. 16S sequencing",xlab="16S rRNA gene tag sequencing",ylab="MetaPhlAn results from metagenomic sequencing")
-dev.off()
-
 cor(otu.tab.common.effect, y = d.common.effect, use = "everything", method = "spearman")
 # [1] 0.4456304
+
+common.genus <- rownames(h.metnash.d.select.aldex)[which(rownames(h.metnash.d.select.aldex) %in% rownames(h.metnash.aldex))]
+otu.tab.common.effect <- h.metnash.aldex$effect[match(common.genus,rownames(h.metnash.aldex))]
+d.select.common.effect <- h.metnash.d.select.aldex$effect[match(common.genus,rownames(h.metnash.d.select.aldex))]
+plot(otu.tab.common.effect, d.select.common.effect, pch=19,col=mycolor, main="Effect sizes of healthy vs extreme NASH\nfor MetaPhlAn results vs. 16S sequencing",xlab="16S rRNA gene tag sequencing",ylab="MetaPhlAn results from metagenomic sequencing")
+cor(otu.tab.common.effect, y = d.select.common.effect, use = "everything", method = "spearman")
+# [1] 0.5193331
+dev.off()
+
+
